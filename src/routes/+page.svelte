57ed83283d9217dda9f02b887136777f27f0b488
KevinW90 <kevinjwilliams76@gmail.com>
2023-12-15 13:59:28 -0500

    uploaded all needed images

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,12 @@
 <script>
+	// import some images to test
+	import iconSpeechBubbles from '../lib/images/icon-speech-bubbles.png';
+	import iconTarget from '../lib/images/icon-target.png';
+	import iconCalendar from '../lib/images/icon-calendar.png';
+	import iconTrust from '../lib/images/icon-trust.png';
+	import iconLocation from '../lib/images/icon-location.png';
+	import iconAt from '../lib/images/icon-at.png';
+	import iconPhone from '../lib/images/icon-phone.png';
 </script>
 
 <svelte:head>
@@ -7,6 +15,10 @@
 </svelte:head>
 
 <section>
+	<picture>
+		<source srcset={iconSpeechBubbles} type="image/png" />
+		<img src={iconSpeechBubbles} alt="icon-speech-bubbles" />
+	</picture>
 	
 
 </section>
