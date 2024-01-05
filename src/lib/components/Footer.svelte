05f2d3c29b4a97fdd4c39aac733ec8c7cb4cdc93
KevinW90 <kevinjwilliams76@gmail.com>
2024-01-05 09:59:27 -0500

    footer

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+	import Icon from '@iconify/svelte';
+
+	let year = new Date().getFullYear();
+</script>
+
+<div class="footer">
+	<span>
+		<Icon icon="tabler:copyright" />
+		{year} DoggyDays
+	</span>
+	<span>301 646 4398</span>
+</div>
+
+<style>
+	.footer {
+		background-color: var(--color-black);
+		color: var(--color-white);
+		font-family: 'Fredeoka One';
+
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		gap: 1rem;
+
+		padding: 0.5rem 0;
+	}
+
+	span {
+		display: flex;
+		align-items: center;
+	}
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,28 +2,29 @@
 	import '../app.css';
 	import '@fontsource/fredoka-one';
 
-	import Header from "$lib/components/Header.svelte";
-	import { isMobile } from "$lib/stores";
-	import { onMount } from "svelte";
+	import Header from '$lib/components/Header.svelte';
+	import { isMobile } from '$lib/stores';
+	import { onMount } from 'svelte';
+	import Footer from '$lib/components/Footer.svelte';
 
 	let _isMobile = true;
-// resize event listener
-const handleResize = () => {
-	// change value of isMobile if window width is greater than 768px
-	_isMobile = window.innerWidth < 867;
-}
+	// resize event listener
+	const handleResize = () => {
+		// change value of isMobile if window width is greater than 768px
+		_isMobile = window.innerWidth < 867;
+	};
 
-$: isMobile.set(_isMobile);
+	$: isMobile.set(_isMobile);
 
-// init isMobile flag on mount
-onMount(() => {
-	handleResize();
-	window.addEventListener('resize', handleResize);
+	// init isMobile flag on mount
+	onMount(() => {
+		handleResize();
+		window.addEventListener('resize', handleResize);
 
-	return () => {
-		window.removeEventListener('resize', handleResize);
-	}
-});
+		return () => {
+			window.removeEventListener('resize', handleResize);
+		};
+	});
 </script>
 
 <div class="app">
@@ -31,4 +32,5 @@ onMount(() => {
 	<main>
 		<slot />
 	</main>
+	<Footer />
 </div>
